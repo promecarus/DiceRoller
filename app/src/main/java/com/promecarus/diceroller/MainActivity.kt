423f2f29fc17ab75d607b267a66c87150260088c
promecarus <haikalslipi@gmail.com>
2023-08-08 11:43:45 +0700

    finish

--- app/src/main/java/com/promecarus/diceroller/MainActivity.kt
+++ app/src/main/java/com/promecarus/diceroller/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.promecarus.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            findViewById<TextView>(R.id.textView).text = Dice(6).roll().toString()
+        }
     }
 }
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+
