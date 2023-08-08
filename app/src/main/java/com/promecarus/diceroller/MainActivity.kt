a9db4c2121235315b5ad94420534e40ac55f570a
promecarus <haikalslipi@gmail.com>
2023-08-08 12:13:03 +0700

    practice

--- app/src/main/java/com/promecarus/diceroller/MainActivity.kt
+++ app/src/main/java/com/promecarus/diceroller/MainActivity.kt
@@ -12,16 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            findViewById<ImageView>(R.id.imageView).setImageResource(
-                when (Dice(6).roll()) {
-                    1 -> R.drawable.dice_1
-                    2 -> R.drawable.dice_2
-                    3 -> R.drawable.dice_3
-                    4 -> R.drawable.dice_4
-                    5 -> R.drawable.dice_5
-                    else -> R.drawable.dice_6
-                }
-            )
+            findViewById<ImageView>(R.id.imageView1).setImageResource(Dice(6).image())
+            findViewById<ImageView>(R.id.imageView2).setImageResource(Dice(6).image())
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
         }
     }
@@ -30,5 +22,16 @@ class MainActivity : AppCompatActivity() {
 class Dice(private val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
+    }
+
+    fun image(): Int {
+        return when (roll()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 }
